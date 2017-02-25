0b995f63c71038514a29e4031a2edca74083cca4
AnupKumarGupta <reachanupgupta@gmail.com>
2017-02-25 23:59:36 +0530

    added data.py| for managing parsing of data

---
+++ data.py
@@ -0,0 +1,13 @@
+#SPECIAL TOKENS
+#
+#Usage
+#=====
+# Used to identify start and end of documents, paragraphs and sentences
+PARAGRAPH_START = '<p>'
+PARAGRAPH_END = '</p>'
+SENTENCE_START = '<s>'
+SENTENCE_END = '</s>'
+UNKNOWN_TOKEN = '<UNK>'
+PAD_TOKEN = '<PAD>'
+DOCUMENT_START = '<d>'
+DOCUMENT_END = '</d>'
