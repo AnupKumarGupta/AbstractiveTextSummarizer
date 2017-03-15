979ff5e9c58275995a012aee868ef53b587fccd6
AnupKumarGupta <reachanupgupta@gmail.com>
2017-03-15 18:00:17 +0530

    next_batch added | Information link about bucketing added

--- batch_reader.py
+++ batch_reader.py
@@ -2,6 +2,8 @@ import Queue
 from collections import namedtuple
 from threading import Thread
 
+import numpy as np
+
 ModelInput = namedtuple('ModelInput', 'enc_input, dec_input, target, enc_len, dec_len, origin_article,origin_abstract')
 
 BUCKET_CACHE_BATCH = 100
@@ -9,10 +11,8 @@ QUEUE_NUM_BATCH = 100
 
 
 class Batcher(object):
-    """Batch reader with shuffling and bucketing support."""
-
-    def __init__(self, data_path, vocab, hps, article_key, abstract_key, max_article_sentences,
-                 max_abstract_sentences, bucketing=True, truncate_input=False):
+    def __init__(self, data_path, vocab, hps, article_key, abstract_key, max_article_sentences, max_abstract_sentences,
+                 bucketing=True, truncate_input=False):
 
         """Batcher constructor.
 
@@ -25,8 +25,11 @@ class Batcher(object):
           max_article_sentences: Max number of sentences used from article.
           max_abstract_sentences: Max number of sentences used from abstract.
           bucketing: Whether bucket articles of similar length into the same batch.
-          truncate_input: Whether to truncate input that is too long. Alternative is
-            to discard such examples.
+          truncate_input: Whether to truncate input that is too long. Alternative is to discard such examples.
+
+        Information:
+           bucketing: https://www.tensorflow.org/tutorials/seq2seq#bucketing_and_padding
+
         """
         self._data_path = data_path
         self._vocab = vocab
@@ -37,8 +40,9 @@ class Batcher(object):
         self._max_abstract_sentences = max_abstract_sentences
         self._bucketing = bucketing
         self._truncate_input = truncate_input
+
         self._input_queue = Queue.Queue(QUEUE_NUM_BATCH * self._hps.batch_size)
-        self._bucket_input_queue = Queue.Queue(QUEUE_NUM_BATCH)
+        self._bucket_input_queue = Queue.Queue(BUCKET_CACHE_BATCH)
         self._input_threads = []
 
         for _ in xrange(16):
@@ -46,6 +50,7 @@ class Batcher(object):
             self._input_threads[-1].daemon = True
             self._input_threads[-1].start()
         self._bucketing_threads = []
+
         for _ in xrange(4):
             self._bucketing_threads.append(Thread(target=self._fill_bucket_input_queue))
             self._bucketing_threads[-1].daemon = True
@@ -55,6 +60,58 @@ class Batcher(object):
         self._watch_thread.daemon = True
         self._watch_thread.start()
 
+    """Batch reader with shuffling and bucketing support."""
+
+    def next_batch(self):
+        """Returns a batch of inputs for seq2seq attention model.
+
+        Returns:
+          enc_batch: A batch of encoder inputs [batch_size, hps.enc_timestamps].
+          dec_batch: A batch of decoder inputs [batch_size, hps.dec_timestamps].
+          target_batch: A batch of targets [batch_size, hps.dec_timestamps].
+          enc_input_len: encoder input lengths of the batch.
+          dec_input_len: decoder input lengths of the batch.
+          loss_weights: weights for loss function, 1 if not padded, 0 if padded.
+          origin_articles: original article words.
+          origin_abstracts: original abstract words.
+        """
+        # region Initialization of vectors with zeros or 'None'
+
+        enc_batch = np.zeros(self._hps.batch_size, self._hps.enc_timesteps, dtype=np.int32)
+        enc_input_lens = np.zeros(self._hps.batch_size, dtype=np.int32)
+        dec_batch = np.zeros(self._hps.batch_size, self._hps.dec_timesteps, dtype=np.int32)
+        dec_output_lens = np.zeros(self._hps.batch_size, dtype=np.int32)
+        target_batch = np.zeros(self._hps.batch_size, self._hps.dec_timesteps, dtype=np.int32)
+        loss_weights = np.zeros(self._hps.batch_size, self._hps.dec_timesteps, dtype=np.float32)
+
+        # y = ['None'] * 3
+        # print y
+        # ['None', 'None', 'None']
+
+        origin_articles = ['None'] * self._hps.batch_size
+        origin_abstracts = ['None'] * self._hps.batch_size
+        # endregion
+
+        buckets = self._bucket_input_queue.get()
+
+        for i in xrange(self._hps.batch_size):
+
+            (enc_inputs, dec_inputs, targets, enc_input_len, dec_output_len, article, abstract) = buckets[i]
+
+            origin_articles[i] = article
+            origin_abstracts[i] = abstract
+            enc_input_lens[i] = enc_input_len
+            dec_output_lens[i] = dec_output_len
+            enc_batch[i, :] = enc_inputs[:]
+            dec_batch[i, :] = dec_inputs[:]
+            target_batch[i, :] = targets[:]
+
+            for j in xrange(dec_output_len):
+                loss_weights[i][j] = 1
+
+        return (enc_batch, dec_batch, target_batch, enc_input_lens, dec_output_lens, loss_weights, origin_articles,
+                origin_abstracts)
+
     def _fill_input_queue(self):
         pass
 
@@ -63,3 +120,4 @@ class Batcher(object):
 
     def _watch_threads(self):
         pass
+
