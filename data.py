b88f5aece18f6c5b5c1adf8c31e25b7cc72f9346
AnupKumarGupta <reachanupgupta@gmail.com>
2017-03-16 18:26:40 +0530

    example_gen added

--- data.py
+++ data.py
@@ -5,7 +5,12 @@
 # Used to identify start and end of documents, paragraphs and sentences
 #
 
+import glob
+import random
+import struct
 import sys
+
+from tensorflow.core.example import example_pb2
 
 PARAGRAPH_START = '<p>'
 PARAGRAPH_END = '</p>'
@@ -136,6 +141,43 @@ def paragraph_to_sentences(paragraph, include_token=True):
     """
     s_gen = snippet_generator(paragraph, SENTENCE_START, SENTENCE_END, include_token)
     return [s for s in s_gen]
+
+
+def example_gen(data_path, num_epochs=None):
+    """
+      Generates tf.Examples from path of data files.
+
+      Binary data format: <length><blob>. <length> represents the byte size
+      of <blob>. <blob> is serialized tf.Example proto. The tf.Example contains
+      the tokenized article text and summary.
+
+    Args:
+      data_path: path to tf.Example data files.
+      num_epochs: Number of times to go through the data. None means infinite.
+
+    Yields:
+      Deserialized tf.Example.
+
+    If there are multiple files specified, they accessed in a random order.
+
+    """
+    epoch = 0
+    while True:
+        if num_epochs is not None and epoch >= num_epochs:
+            break
+        filelist = glob.glob(data_path)
+        assert filelist, 'Empty filelist.'
+        random.shuffle(filelist)
+        for f in filelist:
+            reader = open(f, 'rb')
+            while True:
+                len_bytes = reader.read(8)
+                if not len_bytes: break
+                str_len = struct.unpack('q', len_bytes)[0]
+                example_str = struct.unpack('%ds' % str_len, reader.read(str_len))[0]
+                yield example_pb2.Example.FromString(example_str)
+
+        epoch += 1
 
 
 def snippet_generator(text, start_tok, end_tok, inclusive=True):
