8a7879ed0c413c0ea956dfb696f7ee6378788fac
AnupKumarGupta <reachanupgupta@gmail.com>
2017-03-16 18:34:01 +0530

    Code updated

--- batch_reader.py
+++ batch_reader.py
@@ -143,15 +143,13 @@ class Batcher(object):
 
             # Filter out too-short input
             if len(enc_inputs) < self._hps.min_input_len or len(dec_inputs) < self._hps.min_input_len:
-                tf.logging.warning('Drop an example - too short.\nenc:%d\ndec:%d',
-                                   len(enc_inputs), len(dec_inputs))
+                tf.logging.warning('Drop an example - too short.\nenc:%d\ndec:%d', len(enc_inputs), len(dec_inputs))
                 continue
 
             # If we're not truncating input, throw out too-long input
             if not self._truncate_input:
                 if len(enc_inputs) > self._hps.enc_timesteps or len(dec_inputs) > self._hps.dec_timesteps:
-                    tf.logging.warning('Drop an example - too long.\nenc:%d\ndec:%d',
-                                       len(enc_inputs), len(dec_inputs))
+                    tf.logging.warning('Drop an example - too long.\nenc:%d\ndec:%d', len(enc_inputs), len(dec_inputs))
                     continue
 
             # If we are truncating input, do so if necessary
@@ -179,9 +177,8 @@ class Batcher(object):
             while len(targets) < self._hps.dec_timesteps:
                 targets.append(end_id)
 
-            element = ModelInput(enc_inputs, dec_inputs, targets, enc_input_len,
-                                 dec_output_len, ' '.join(article_sentences),
-                                 ' '.join(abstract_sentences))
+            element = ModelInput(enc_inputs, dec_inputs, targets, enc_input_len, dec_output_len,
+                                 ' '.join(article_sentences), ' '.join(abstract_sentences))
             self._input_queue.put(element)
 
     def _fill_bucket_input_queue(self):
